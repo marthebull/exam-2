394f9408ea4c7c101627ecd61e2ae8337e6f5c3f
marthebull <marthebullpettersen@gmail.com>
2023-05-04 00:27:13 +0200

    lists cards with data

--- src/components/cards/list/index.js
+++ src/components/cards/list/index.js
@@ -2,47 +2,54 @@ import { Link } from "react-router-dom";
 import { ButtonOutlineWhite } from "../../../styles/GlobalStyles";
 import { useGetVenuesQuery } from "../../../state/api/api";
 
-const ListCard = () => {
+const ListCards = () => {
   const { data: venues, isLoading: venuesIsLoading } = useGetVenuesQuery();
   console.log(useGetVenuesQuery());
 
   return (
-    <Link to="/venue/:id">
-      <div className="full-w rounded overflow-hidden shadow-md ">
-        <div className="relative overflow-hidden bg-no-repeat h-80">
-          <img
-            src="https://images.unsplash.com/photo-1470229678033-3a6dc3d6bf76?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"
-            alt="Sunset in the mountains"
-          ></img>
-          <div className="flex flex-col justify-center align-middle absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-dark bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100">
-            <ButtonOutlineWhite className="opacity-1">
-              view venue
-            </ButtonOutlineWhite>
-          </div>
-        </div>
-        <div className="px-3 py-4">
-          <h3 className="mb-2 h3">Placencia Village, Belize</h3>
-          <div className="flex flex-row gap-3">
-            <img
-              className="icon"
-              src="/images/moon-sea-icon.svg"
-              alt="Night icon"
-            ></img>
+    <div className="w-100 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 pb-6">
+      {venues.map((venues) => (
+        <Link to="/venues/:id">
+          <div className="full-w rounded overflow-hidden shadow-md ">
+            <div
+              className="relative overflow-hidden bg-no-repeat h-80"
+              style={{
+                backgroundImage: `url(${venues.media})`,
+                backgroundPosition: `center`,
+                backgroundSize: `cover`,
+              }}
+            >
+              <div className="flex flex-col justify-center align-middle absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-dark bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100">
+                <ButtonOutlineWhite className="opacity-1">
+                  view venue
+                </ButtonOutlineWhite>
+              </div>
+            </div>
+            <div className="px-3 py-4">
+              <h3 className="mb-2 h3">Placencia Village, Belize</h3>
+              <div className="flex flex-row gap-3">
+                <img
+                  className="icon"
+                  src="/images/moon-sea-icon.svg"
+                  alt="Night icon"
+                ></img>
 
-            <p>48 668 NOK</p>
+                <p>48 668 NOK</p>
+              </div>
+              <div className="flex flex-row gap-3">
+                <img
+                  className="icon"
+                  src="/images/people-icon.svg"
+                  alt="Guests"
+                ></img>
+                <p>2</p>
+              </div>
+            </div>
           </div>
-          <div className="flex flex-row gap-3">
-            <img
-              className="icon"
-              src="/images/people-icon.svg"
-              alt="Guests"
-            ></img>
-            <p>2</p>
-          </div>
-        </div>
-      </div>
-    </Link>
+        </Link>
+      ))}
+    </div>
   );
 };
 
-export default ListCard;
+export default ListCards;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { ButtonSolidDark, Container } from "../../styles/GlobalStyles";
 import { Link } from "react-router-dom";
-import ListVenuesGrid from "../../components/grids/ListVenuesGrid";
 import HeroHome from "../../components/hero-section/HeroHome";
+import ListCards from "../../components/cards/list";
 
 const Home = () => {
   return (
@@ -10,7 +10,7 @@ const Home = () => {
       <HeroHome />
       <Container className="flex flex-col gap-6">
         <h2 className="h2 my-8">Don't snooze on these</h2>
-        <ListVenuesGrid />
+        <ListCards />
         <Link to="/venues">
           <ButtonSolidDark>see all venues</ButtonSolidDark>
         </Link>
