75d7e3b1e047817835509ca7b67e84e075528013
marthebull <marthebullpettersen@gmail.com>
2023-05-05 00:49:35 +0200

    grid works agaiin

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     screens: {
+      xs: "320px",
+      sm: "375px",
+      lsm: "425px",
       md: "700px",
+      lg: "1024px",
+      xl: "1280px",
     },
     extend: {},
   },
