4c10cdf744dbaf317b5f21bca4476b93e2b362bc
marthebull <marthebullpettersen@gmail.com>
2023-05-08 10:51:26 +0200

    card loading

--- src/components/cards/list/AllVenues.js
+++ src/components/cards/list/AllVenues.js
@@ -57,29 +57,25 @@ const ListAllVenues = () => {
             src="/images/breakfast.svg"
             alt="Breakfast filter icon"
             className="medium-icon"
-            style={{ cursor: "pointer", opacity: criteria.breakfast ? 1 : 0.5 }}
-            onClick={() => handleCriteriaChange("breakfast")}
+            style={{ cursor: "pointer", opacity: 0.5 }}
           ></img>
           <img
             src="/images/parking.svg"
             alt="Parking filter icon"
             className="medium-icon"
-            style={{ cursor: "pointer", opacity: criteria.parking ? 1 : 0.5 }}
-            onClick={() => handleCriteriaChange("parking")}
+            style={{ cursor: "pointer", opacity: 0.5 }}
           ></img>
           <img
             src="/images/pets.svg"
             alt="Pets filter icon"
             className="medium-icon"
-            style={{ cursor: "pointer", opacity: criteria.pets ? 1 : 0.5 }}
-            onClick={() => handleCriteriaChange("pets")}
+            style={{ cursor: "pointer", opacity: 0.5 }}
           ></img>
           <img
             src="/images/wifi.svg"
             alt="Wifi filter icon"
             className="medium-icon"
-            style={{ cursor: "pointer", opacity: criteria.wifi ? 1 : 0.5 }}
-            onClick={() => handleCriteriaChange("wifi")}
+            style={{ cursor: "pointer", opacity: 0.5 }}
           ></img>
         </FilterWrapper>
         <div className="w-100 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 pb-6">
