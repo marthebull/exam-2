921e9437d731f4149c58cb63a8ef2b24faec86d4
marthebull <marthebullpettersen@gmail.com>
2023-05-29 15:13:15 +0200

    Update BookingCard.js

--- src/components/cards/dashboard/BookingCard.js
+++ src/components/cards/dashboard/BookingCard.js
@@ -66,7 +66,7 @@ const BookingCard = ({ user }) => {
                     alt="Night icon"
                   ></img>
                   <p className="a">
-                    {getDateDifference(booking?.dateFrom, booking?.dateTo)}
+                    {getDateDifference(booking?.dateFrom, booking?.dateTo)}{" "}
                     nights
                   </p>
                 </div>
